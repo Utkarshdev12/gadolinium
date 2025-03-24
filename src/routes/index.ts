907232e4309a4c3c38583e65a1b41ef0ed931aa2
Utkarsh <utkarshdev12@gmail.com>
2025-03-24 16:53:29 +0530

    initialize project structure with authentication and database setup

---
+++ src/routes/index.ts
@@ -0,0 +1,43 @@
+import { Hono } from "hono";
+import { signUpWithUsernameAndPassword } from "../controllers/authentication";
+import { SignUpWithUsernameAndPasswordError } from "../controllers/authentication/+type";
+
+export const hono = new Hono();
+hono.post("/authentication/sign-up", async (context) => {
+  const {  username, password } = await context.req.json()  ; 
+  try{
+    const result=await signUpWithUsernameAndPassword({
+        username,
+        password,
+      });
+      return context.json(
+        {
+          data: result,
+        },201
+      );
+    }catch(e){
+    if(e===SignUpWithUsernameAndPasswordError.CONFLICTING_USERNAME){
+      return context.json(
+        {
+          message: "Username already exists",
+        },
+        409
+      );
+      }
+      return context.json(
+        { 
+          message: "Unknown error",
+          },
+          500
+        );
+      }
+    }
+  );
+hono.get("/health", (context) => {
+  return context.json(
+    {
+      message: "All Ok",
+    },
+    200
+  );
+});
