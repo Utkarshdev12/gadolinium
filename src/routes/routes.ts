72df7601351bb4e6d5d0e604595168c1be8b0288
Utkarsh <utkarshdev12@gmail.com>
2025-03-25 15:24:45 +0530

    implement user routes and token middleware: add user retrieval endpoints and JWT authentication middleware

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,14 @@
 import { Hono } from "hono";
-
 import { authenticationRoutes } from "./authentication-routes";
+import { usersRoutes } from "./users-routes";
+import { logger } from "hono/logger";
 
 export const allRoutes = new Hono();
 
-allRoutes.route("/authentication", authenticationRoutes );
+allRoutes.use(logger());
 
-
+allRoutes.route("/authentication", authenticationRoutes);
+allRoutes.route("/users", usersRoutes);
 
 allRoutes.get("/health", (context) => {
   return context.json(
