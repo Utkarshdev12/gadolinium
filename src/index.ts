4efbc33113b965e6d1352aea844ee860ad125e6c
Utkarsh <utkarshdev12@gmail.com>
2025-03-25 10:32:54 +0530

    refactor authentication module: consolidate sign-up and log-in logic, remove deprecated files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { hono } from "./routes";
+import { hono } from "./routes/routes";
 import { serve } from "@hono/node-server";
 
 serve(hono, (info) => {
-  console.log(`Server is running on port ${info.port}`);
+  console.log(`Server is running @ http://localhost:${info.port}`);
 });
